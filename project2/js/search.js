cb429e392f590e04ac3221c5fe953c3a0e7483df
YedTM <rcc3452@rit.edu>
2023-11-14 17:14:02 -0500

    Somehow finished the basic code for getting the breeds and subbreeds to appear. Have to add comments and touch up look a bit
    Look into fixing the spacing issue when there are only images and no text

--- project2/js/search.js
+++ project2/js/search.js
@@ -1,5 +1,6 @@
 window.onload = (e) => {document.querySelector("#search").onclick = searchButtonClicked};
 let displayTerm = "";
+let displaySubTerm = "";
 
 function searchButtonClicked(){
     console.log("searchButtonClicked() called");
@@ -17,12 +18,32 @@ function searchButtonClicked(){
 
     if (term.length < 1) return;
 
-    url += term + "/images/random/";
+    let subTerm = document.querySelector("#sub-selector").value;
+    displaySubTerm = subTerm;
+    subTerm = subTerm.trim();
+    subTerm = encodeURIComponent(subTerm);
+
+    url += term 
+
+    if (subTerm.length > 1)
+    {
+        url += "/" + subTerm;
+    }
+
+    url += "/images/random/";
 
     let limit = document.querySelector("#limit").value;
     url += limit;
     
-    document.querySelector("#status").innerHTML = "<b>Searching for '" + displayTerm + "'</b>";
+    if (displaySubTerm.length > 1)
+    {
+        document.querySelector("#status").innerHTML = "<b>Searching for '" + displaySubTerm + " " + displayTerm + "'</b>";
+    }
+    else
+    {
+        document.querySelector("#status").innerHTML = "<b>Searching for '" + displayTerm + "'</b>";
+    }
+    
 
     console.log(url);
 
@@ -45,34 +66,149 @@ function dataLoaded(e){
     console.log(xhr.responseText);
 
     let obj = JSON.parse(xhr.responseText);
-    if(!obj.data || obj.data.length == 0){
+    if(!obj.message || obj.message.length == 0){
         document.querySelector("#status").innerHTML = "<b>No results found for '" + displayTerm + "'</b>";
         return;
     }
 
-    let results = obj.data;
+    let results = obj.message;
     console.log("results.length = " + results.length);
     let bigString = "";
 
     for (let i=0;i<results.length;i++){
         let result = results[i];
 
-        let smallURL = result.images.fixed_width.url;
-        if (!smallURL) smallURL = "images/no-image-found.png";
+        let smallURL = result;
 
-        let url = result.url;
-
-
-        let line = `<div class='result'>Rating: ${result.rating.toUpperCase()}<br /><img src='${smallURL}' title= '${result.id}' />`;
-        line += `<span><a target='_blank' href='${url}'>View on Giphy</a></span></div>`;
+        let line = `<div class='result'><img src='${smallURL}' title= '${smallURL}' />`;
+        line += `<span><a target='_blank' href='${smallURL}'>View On Its Own</a></span></div>`;
 
         bigString += line;
     }
 
     document.querySelector("#content").innerHTML = bigString;
-    document.querySelector("#status").innerHTML = "<b>Success!</b><p><i>Here are " + results.length + " results for '" + displayTerm + "'</i></p>";
+    if (displaySubTerm.length > 1)
+    {
+        document.querySelector("#status").innerHTML = "<b>Success!</b><p><i>Here are " + results.length + " results for '" + displaySubTerm + " " + displayTerm + "'</i></p>";
+    }
+    else
+    {
+        document.querySelector("#status").innerHTML = "<b>Success!</b><p><i>Here are " + results.length + " results for '" + displayTerm + "'</i></p>";
+    }
 }
 
 function dataError(e){
     console.log("An error occured");
 }
+
+function getArray(url){
+    let xhr = new XMLHttpRequest();
+
+    xhr.onload = createInitialArray;
+
+    xhr.onerror = dataError;
+
+    xhr.open("GET", url);
+    xhr.send();
+}
+
+function createInitialArray(e){
+    let xhr = e.target;
+    // console.log(xhr.responseText);
+
+    let obj = JSON.parse(xhr.responseText);
+    // console.log(obj);
+    // console.log(obj.message);
+    let results = obj.message;
+    
+    let userInitialSelection = document.querySelector("#dog-selector");
+    for (dog in results)
+    {
+        let breed = document.createElement("option");
+        breed.text = dog;
+        userInitialSelection.add(breed);
+        if (dog == userInitialSelection.value)
+        {
+            let subURL = "http://dog.ceo/api/breed/";
+            subURL += dog;
+            subURL += "/list";
+            getSubArray(subURL);
+        }
+    }
+
+}
+
+function getChangeArray(url){
+    let xhr = new XMLHttpRequest();
+
+    xhr.onload = createChangeArray;
+
+    xhr.onerror = dataError;
+
+    xhr.open("GET", url);
+    xhr.send();
+}
+
+function createChangeArray(e){
+    let xhr = e.target;
+    // console.log(xhr.responseText);
+
+    let obj = JSON.parse(xhr.responseText);
+    // console.log(obj);
+    // console.log(obj.message);
+    let results = obj.message;
+    
+    let userInitialSelection = document.querySelector("#dog-selector");
+    for (dog in results)
+    {
+        if (dog == userInitialSelection.value)
+        {
+            let subURL = "http://dog.ceo/api/breed/";
+            subURL += dog;
+            subURL += "/list";
+            getSubArray(subURL);
+        }
+    }
+}
+
+function getSubArray(url){
+    let xhr = new XMLHttpRequest();
+
+    xhr.onload = createSubArray;
+
+    xhr.onerror = dataError;
+
+    xhr.open("GET", url);
+    xhr.send();
+}
+
+function createSubArray(e){
+    let xhr = e.target;
+    console.log(xhr.responseText);
+
+    let obj = JSON.parse(xhr.responseText);
+    console.log(obj);
+    console.log(obj.message);
+    let results = obj.message;
+    console.log(results[0]);
+    
+    let userSecondarySelection = document.querySelector("#sub-selector");
+    if (userSecondarySelection.length > 0)
+    {
+        for (let i = userSecondarySelection.length - 1; i >= 0; i--)
+        {
+            userSecondarySelection.remove(i);
+        }
+    }
+    for (let i = 0; i < results.length; i++)
+    {
+        let breed = document.createElement("option");
+        breed.text = results[i];
+        userSecondarySelection.add(breed);
+    }
+}
+
+document.onload = getArray("https://dog.ceo/api/breeds/list/all");
+
+
+
