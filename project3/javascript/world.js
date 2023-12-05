d8af0d374b31ec0b87923f3cb659b97e66f14022
YedTM <rcc3452@rit.edu>
2023-12-05 00:15:36 -0500

    Finished a decent portion of the world layout
    Work on events and maybe second game

--- project3/javascript/world.js
+++ project3/javascript/world.js
@@ -1,46 +1,48 @@
 let worldLayout = {
     layout: [
         [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+        [1,0,0,0,0,0,0,2,2,2,0,0,0,0,0,0,1],
+        [1,2,2,2,2,0,0,0,0,0,0,0,0,0,0,0,1],
         [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,2,2,2,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,2,2,2,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,2,2,2,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,1],
+        [1,2,2,2,0,0,0,0,0,0,0,0,0,0,0,2,1],
+        [1,0,0,0,0,0,0,2,2,2,0,0,0,0,0,2,1],
+        [1,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,2,2,2,0,0,1],
         [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
     ]
 };
 
 // these map to the CSS classes in main.css
-const cssClass = Object.freeze({
-	CHEST1: 	"chest1",
+const cssClass = {
+	LOCKED: 	"locked",
 	KEY1: 		"key1",
 	MONSTER1: 	"monster1",
-	MONSTER2: 	"monster2",
-	MONSTER3: 	"monster3",
+	MONSTER2:	"monster2",
 	PLAYER: 	"player",
-	RING1: 		"ring1",
-	TREASURE1: 	"treasure1",
-	TREASURE2: 	"treasure2"
-});
+	PORTAL: 	"portal",
+	POWER1: 	"power1",
+	POWER2: 	"power2",
+	POWER3:		"power3"
+};
 
 const allGameObjects = {
 	// we aren't using .type yet
 	level1:[
 		{x:10, y:5, type:"monster", 	className: cssClass.MONSTER1},
 		{x:12, y:5, type:"monster",	className: cssClass.MONSTER1},
-		{x:25, y:15, type:"monster",	className: cssClass.MONSTER2},
-		{x:10, y:3,  type:"monster",	className: cssClass.MONSTER3},
-		{x:24, y:15, type:"chest",		className: cssClass.CHEST1},
+		{x:5, y:1, type:"monster",	className: cssClass.MONSTER2},
+		{x:4, y:9, type:"monster",	className: cssClass.MONSTER2},
+		{x:14, y:10, type:"monster",	className: cssClass.MONSTER2},
+		{x:15, y:11, type:"locked",		className: cssClass.LOCKED},
 		{x:1, y:9,  type:"key",		className: cssClass.KEY1},
-		{x:17, y:5,  type:"treasure",	className: cssClass.TREASURE1},
-		{x:2,  y:17, type:"treasure",	className: cssClass.TREASURE2},
-		{x:10, y:16, type:"ring",		className: cssClass.RING1}
+		{x:1, y:11,  type:"power",	className: cssClass.POWER1},
+		{x:1,  y:3, type:"power",	className: cssClass.POWER2},
+		{x:15,  y:1, type:"power",	className: cssClass.POWER3},
+		{x:1, y:1, type:"portal",		className: cssClass.PORTAL}
 	]
 }
 
