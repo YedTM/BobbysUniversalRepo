d43e40c2ce8fdc885377f447afd4e27cc40e6a26
YedTM <rcc3452@rit.edu>
2023-12-06 23:38:56 -0500

    Did a lot of work on getting movement to work and removing objects
    Objects in the world can now be picked up. There is a win message for the world. Can now spawn in projectiles in battle. Need to work on things moving tomorrow

--- project3/javascript/battle.js
+++ project3/javascript/battle.js
@@ -1,6 +1,6 @@
 let worldLayout = {
     layout: [
-        [3,1,1,1,1,1,1,1,1,1,1,1,1,1,3],
+        [3,1,1,1,1,1,1,1,1,1,1,1,1,1,4],
         [2,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
         [2,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
         [2,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
@@ -16,7 +16,7 @@ let worldLayout = {
         [2,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
         [2,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
         [2,0,0,0,0,0,0,0,0,0,0,0,0,0,2],
-        [3,1,1,1,1,1,1,1,1,1,1,1,1,1,3]
+        [6,1,1,1,1,1,1,1,1,1,1,1,1,1,5]
 
     ]
 };
@@ -31,16 +31,20 @@ const cssClass = {
 	PORTAL: 	"portal",
 	POWER1: 	"power1",
 	POWER2: 	"power2",
-	POWER3:		"power3"
+	POWER3:		"power3",
+    PLAYER_SHOT:"pShot",
+	ENEMY_SHOT: "eShot"
 };
 
 const allGameObjects = {
 	// we aren't using .type yet
 	level1:[
 		{x:10, y:5, type:"monster", 	className: cssClass.MONSTER1},
-		{x:12, y:5, type:"monster",	className: cssClass.MONSTER1},
-		{x:1, y:9,  type:"power",	className: cssClass.POWER1},
-		{x:1,  y:3, type:"power",	className: cssClass.POWER2},
-		{x:11,  y:5, type:"power",	className: cssClass.POWER3},
-	]
+		{x:12, y:5, type:"monster",	className: cssClass.MONSTER1}
+	],
+    playerShot: {x:5,  y:5, type:"shot",	className: cssClass.PLAYER_SHOT},
+    enemyShot: {x:-1,  y:-1, type:"shot",	className: cssClass.ENEMY_SHOT},
+    powerOne: {x:1, y:9,  type:"power",	className: cssClass.POWER1},
+    powerTwo: {x:6,  y:6, type:"power",	className: cssClass.POWER2},
+    powerThree: {x:12,  y:9, type:"power",	className: cssClass.POWER3}
 }
