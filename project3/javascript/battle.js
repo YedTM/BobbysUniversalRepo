4bc8bdb772c57310b907b902b016e9b98e906805
YedTM <rcc3452@rit.edu>
2023-12-15 04:15:57 -0500

    Finished the game, just need to comment
    Comment battle and edit the documentation page. Then you're done buddy! Good job!

--- project3/javascript/battle.js
+++ project3/javascript/battle.js
@@ -1,3 +1,7 @@
+// This creates the grid that the game's tiles are placed
+// The 0s are floors, 1s are horizontal walls, 2s are vertical walls,
+// and 3-6 are all corner walls that have a different rotation depending on
+// the number
 let worldLayout = {
     layout: [
         [3,1,1,1,1,1,1,1,1,1,1,1,1,1,4],
@@ -21,7 +25,8 @@ let worldLayout = {
     ]
 };
 
-// these map to the CSS classes in main.css
+// These are used to create classes in the span that
+// allows the gameObjects to be printed on screen
 const cssClass = {
 	LOCKED: 	"locked",
 	KEY1: 		"key1",
@@ -35,11 +40,14 @@ const cssClass = {
 	ENEMY_SHOT: "eShot"
 };
 
+// The gameObjects are created and placed into allGameObjects in order to
+// printed onto the screen of battle.html.
+// (It was planned to have classes, similar to world.js, but they would constantly ruin 
+// the code present in battle.html so ultimately they were scrapped)
 const allGameObjects = {
-	// we aren't using .type yet
 	level1:[
-		{x:8, y:5, type:"monster", className: cssClass.MONSTER1, lives: 1, classPlace: "one"},
-		{x:12, y:5, type:"monster",	className: cssClass.MONSTER1, lives: 1, classPlace: "two"}
+		{x:8, y:5, type:"monster", className: cssClass.MONSTER1, lives: 5, classPlace: "one"},
+		{x:12, y:5, type:"monster",	className: cssClass.MONSTER1, lives: 5, classPlace: "two"}
 	],
     playerShot: {x:5,  y:5, type:"shot",	className: cssClass.PLAYER_SHOT},
     enemyShot: {x:-1,  y:-1, type:"shot",	className: cssClass.ENEMY_SHOT, numShot: "shot0"},
--- project3/javascript/world.js
+++ project3/javascript/world.js
@@ -1,3 +1,5 @@
+// This creates the grid that the game's tiles are placed
+// The 0s are floors, the 1s are walls, and the 2s are shelves
 let worldLayout = {
     layout: [
         [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
@@ -16,7 +18,8 @@ let worldLayout = {
     ]
 };
 
-// these map to the CSS classes in main.css
+// These are used to create classes in the span that
+// allows the gameObjects to be printed on screen
 const cssClass = {
 	LOCKED: 	"locked",
 	KEY1: 		"key1",
@@ -29,21 +32,59 @@ const cssClass = {
 	POWER3:		"power3"
 };
 
-const allGameObjects = {
-	// we aren't using .type yet
-	level1:[
-		{x:10, y:5, type:"monster", 	className: cssClass.MONSTER1, classPlace: "one"},
-		{x:12, y:5, type:"monster",	className: cssClass.MONSTER1, classPlace: "two"},
-		{x:3, y:5, type:"monster",	className: cssClass.MONSTER1, classPlace: "three"},
-		{x:5, y:1, type:"monster",	className: cssClass.MONSTER2, classPlace: "one"},
-		{x:4, y:9, type:"monster",	className: cssClass.MONSTER2, classPlace: "two"},
-		{x:14, y:10, type:"monster",	className: cssClass.MONSTER2, classPlace: "three"},
-		{x:15, y:11, type:"locked",		className: cssClass.LOCKED},
-		{x:1, y:9,  type:"key",		className: cssClass.KEY1},
-		{x:1, y:11,  type:"power",	className: cssClass.POWER1},
-		{x:1,  y:3, type:"power",	className: cssClass.POWER2},
-		{x:15,  y:1, type:"power",	className: cssClass.POWER3},
-		{x:1, y:1, type:"portal",		className: cssClass.PORTAL}
-	]
+// This class is what makes up all the fundamentals of the gameObjects
+// They have an x and y value, a type (aka what it is), and a className (one of the
+// values in cssClass).
+class GameObject {
+	constructor (x, y, type, className) {
+		this.x = x;
+		this.y = y;
+		this.type = type;
+		this.className = className;
+	}
+}
+// This class is what makes up a Monster gameObject
+// It uses the constructor from the GameObject class and adds on a 
+// classPlace value, which shows what placement the Monster has in the 
+// elements list (in relation to other Monsters).
+// All monsters are of type "monster" and thus don't require that value to be taken in
+// externally
+class Monster extends GameObject{
+	constructor (x, y, className, classPlace) {
+		super(x, y, "monster", className);
+		this.classPlace = classPlace;
+	}
 }
 
+// All of the gameObjects are then created
+let movingOne = new Monster(10, 5, cssClass.MONSTER1, "one");
+let movingTwo = new Monster(12, 5, cssClass.MONSTER1, "two");
+let movingThree = new Monster(3, 5, cssClass.MONSTER1, "three");
+let stillOne = new Monster(5, 1, cssClass.MONSTER2, "one");
+let stillTwo = new Monster(4, 9, cssClass.MONSTER2, "two");
+let stillThree = new Monster(14, 10, cssClass.MONSTER2, "three");
+let locked = new GameObject(15, 11, "locked", cssClass.LOCKED);
+let key = new GameObject(1, 9, "key", cssClass.KEY1);
+let power1 = new GameObject(1, 11, "power", cssClass.POWER1);
+let power2 = new GameObject(1, 3, "power", cssClass.POWER2);
+let power3 = new GameObject(15, 1, "power", cssClass.POWER3);
+let portal = new GameObject(1, 1, "portal", cssClass.PORTAL);
+
+
+// And then they are placed into allGameObjects, to be read by world.html later
+const allGameObjects = {
+	level1:[
+		{entity: movingOne},
+		{entity: movingTwo},
+		{entity: movingThree},
+		{entity: stillOne},
+		{entity: stillTwo},
+		{entity: stillThree},
+		{entity: locked},
+		{entity: key},
+		{entity: power1},
+		{entity: power2},
+		{entity: power3},
+		{entity: portal}
+	]
+}
